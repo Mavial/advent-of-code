024a4be1f293d902a36e67ad0af12d489be7b3ce
Mavial <santi.mavial@gmail.com>
2023-08-06 17:20:19 +0100

    finished day 4

---
+++ 2022/santi/day4.py
@@ -0,0 +1,33 @@
+from data_loader import DataLoader
+
+data_loader = DataLoader(day=4)
+raw_data = data_loader.load()
+data = [
+    [[int(i) for i in x.split("-")] for x in r.split(",")]
+    for r in raw_data.splitlines()
+]
+
+
+""" PART 1 """
+result = 0
+for line in data:
+    if (
+        line[0][0] <= line[1][0]
+        and line[0][1] >= line[1][1]
+        or line[0][0] >= line[1][0]
+        and line[0][1] <= line[1][1]
+    ):
+        result += 1
+print(result)
+
+""" PART 2 """
+result = 0
+for line in data:
+    if (
+        line[0][0] in range(line[1][0], line[1][1] + 1)
+        or line[0][1] in range(line[1][0], line[1][1] + 1)
+        or line[1][0] in range(line[0][0], line[0][1] + 1)
+        or line[1][1] in range(line[0][0], line[0][1] + 1)
+    ):
+        result += 1
+print(result)
