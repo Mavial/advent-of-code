e8dbd694a6e6aa98ded39a1e944ddcd4996bedf4
Mavial <santi.mavial@gmail.com>
2023-08-11 10:52:53 +0100

    fix: performance test no longer loads unwanted funcs

--- 2022/santi/perftest.py
+++ 2022/santi/perftest.py
@@ -3,13 +3,22 @@
 import sys
 import importlib
 import inspect
+import time
 from timeit import timeit
 
-iterations = 100000
+iterations = 10000
 mod = importlib.import_module(sys.argv[1].strip(".py"))
 
 # Iterate through all functions and call them
+start_time = time.time()
 for name, obj in inspect.getmembers(mod):
-    if inspect.isfunction(obj):
+    if inspect.isfunction(obj) and "part" in name:
         avg_time = timeit(obj, number=iterations)/iterations
-        print(f"Average time for {name} --- {avg_time} seconds ---\n")
+        print(f"\033[32mAverage time for \033[33m{name}\033[32m --- \033[36m{avg_time}\033[32m seconds ---\n\n\033[0m")
+time_elapsed = time.time() - start_time
+
+minutes, sec = divmod(time_elapsed, 60)
+seconds, milliseconds = divmod(sec * 1000, 1000)
+print(f"\033[33mTotal time: {int(minutes):02}:{int(seconds):02}:{int(milliseconds):03}")
+
+
--- 2022/santi/run.py
+++ 2022/santi/run.py
@@ -9,10 +9,10 @@ mod = importlib.import_module(sys.argv[1].strip(".py"))
 
 # Iterate through all functions and call them
 for name, obj in inspect.getmembers(mod):
-    if inspect.isfunction(obj):
-        start_time = time.time()
-        result = obj()
-        time_elapsed = time.time() - start_time
-        print(f"\033[32mFunction '\033[33m{name}\033[32m' finished in --- \033[36m{time_elapsed:.6f}\033[32m seconds ---\n\033[0mResult: {result}\n")
+    if inspect.isfunction(obj) and "part" in name:
+            start_time = time.time()
+            result = obj()
+            time_elapsed = time.time() - start_time
+            print(f"\033[32mFunction \033[33m{name}\033[32m finished in --- \033[36m{time_elapsed:.6f}\033[32m seconds ---\n\033[0mResult: {result}\n")
 
 
