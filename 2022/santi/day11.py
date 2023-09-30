715e8d78954d9b45b4c5cb9432020b3ca5f5ef57
Santiago Martinez-Avial <35010625+Mavial@users.noreply.github.com>
2023-09-30 09:25:00 +0200

    finished: day11 part 2 (#1)

--- 2022/santi/day11.py
+++ 2022/santi/day11.py
@@ -1,6 +1,7 @@
 import operator
 from collections import deque
-from typing import List, Callable, Deque
+from typing import List, Callable, Deque, Tuple
+
 from data_loader import DataLoader
 
 
@@ -16,50 +17,65 @@ def load_data():
     return monkeys
 
 
+MOD_CONSTANT = 1
+
+
 class Monkey:
     monkeys: List["Monkey"] = []
 
     def __init__(
         self, raw_starting_items: str, raw_operation: str, raw_test: List[str]
     ):
+        global MOD_CONSTANT
+
         self._items_held: Deque[int] = self._parse_starting_items(raw_starting_items)
         self._operation: Callable[[int], int] = self._parse_operation(raw_operation)
-        self._test: Callable = self._parse_test(raw_test)
+
+        _test, _divisor = self._parse_test(raw_test)
+        self._test: Callable = _test
+        MOD_CONSTANT *= _divisor
+
         self.items_inspected = 0
         Monkey.monkeys.append(self)
 
     def _parse_starting_items(self, raw_starting_items: str) -> Deque[int]:
         return deque(int(n.replace(",", "")) for n in raw_starting_items[2:])
 
+    def _parse_test(self, raw_test: List[str]) -> Tuple[Callable[[int], int], int]:
+        divisor = int(raw_test[0][3])
+        test_pass = int(raw_test[1][5])
+        test_fail = int(raw_test[2][5])
+        return lambda x: test_pass if x % divisor == 0 else test_fail, divisor
+
     def _parse_operation(self, raw_operation: str) -> Callable[[int], int]:
-        ops = {"+": operator.add, "-": operator.sub, "*": operator.mul}
+        ops = {"+": operator.add, "*": operator.mul}
         return lambda old: ops[raw_operation[4]](
             int(raw_operation[3]) if raw_operation[3] != "old" else old,
             int(raw_operation[5]) if raw_operation[5] != "old" else old,
         )
 
-    def _parse_test(self, raw_test: List[str]) -> Callable[[int], int]:
-        divisor = int(raw_test[0][3])
-        test_pass = int(raw_test[1][5])
-        test_fail = int(raw_test[2][5])
-        return lambda x: test_pass if x % divisor == 0 else test_fail
-
     def add_item(self, item: int):
         self._items_held.append(item)
 
-    def turn(self):
+    def turn(self, part: int = 1):
         while self._items_held:
             item = self._items_held.popleft()
 
             self.items_inspected += 1
 
-            new_item_value = int(self._operation(item) / 3)
+            new_item_value = (
+                self._operation(item) % MOD_CONSTANT
+                if part == 2
+                else self._operation(item) // 3
+            )
+
             new_monkey = self._test(new_item_value)
 
             self.monkeys[new_monkey].add_item(new_item_value)
 
 
 def parse_data(data: List[str]) -> List[Monkey]:
+    Monkey.monkeys = []
     for raw_monkey in data:
         Monkey(
             raw_starting_items=raw_monkey[1],
@@ -75,6 +91,7 @@ data = parse_data(unparsed_data)
 
 
 def part1():
+    data = parse_data(unparsed_data)
     for _ in range(20):
         for monkey in data:
             monkey.turn()
@@ -87,5 +104,20 @@ def part1():
     return highest_items_inspected * second_highest_items_inspected
 
 
+def part2():
+    data = parse_data(unparsed_data)
+    for _ in range(10000):
+        for monkey in data:
+            monkey.turn(part=2)
+    items_inspected_list = [monkey.items_inspected for monkey in data]
+
+    highest_items_inspected = max(items_inspected_list)
+    items_inspected_list.remove(highest_items_inspected)
+    second_highest_items_inspected = max(items_inspected_list)
+
+    return highest_items_inspected * second_highest_items_inspected
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
